4f2e1bc89b9cc049acaa65fc9e136b04eaee73c8
GonsagaSantos <miguelgonsagasantos@gmail.com>
2026-04-15 23:41:47 -0300

    feat(p1): altera tipo de input para number no componente CapturaDados

--- src/CapturaDados.jsx
+++ src/CapturaDados.jsx
@@ -18,24 +18,24 @@ function CapturaDados() {
             <div className="row">
                 <div className="col-6">
                     <label className="form-label small">Valor Inicial (R$)</label>
-                    <input name="valor-inicial" className="form-control" type="text" value={valorInicial} onChange={(e) =>
+                    <input name="valor-inicial" className="form-control" type="number" value={valorInicial} onChange={(e) =>
                         setValorInicial(e.target.value)
                     }></input>
 
                     <label className="form-label mt-2 small">Taxa de Juros (% ao mês)</label>
-                    <input name="taxa-de-juros" className="form-control" type="text" value={taxaJuros} onChange={(e) =>
+                    <input name="taxa-de-juros" className="form-control" type="number" value={taxaJuros} onChange={(e) =>
                         setTaxaJuros(e.target.value)
                     }></input>
                 </div>
 
                 <div className="col-6">
                     <label className="form-label small">Aporte Mensal (R$)</label>
-                    <input name="aporte-mensal" className="form-control" type="text" value={aporteMensal} onChange={(e) =>
+                    <input name="aporte-mensal" className="form-control" type="number" value={aporteMensal} onChange={(e) =>
                         setAporteMensal(e.target.value)
                     }></input>
 
                     <label className="form-label mt-2 small">Período (meses)</label>
-                    <input name="periodo" className="form-control" type="text" value={periodoMeses} onChange={(e) =>
+                    <input name="periodo" className="form-control" type="number" value={periodoMeses} onChange={(e) =>
                         setPeriodoMeses(e.target.value)
                     }></input>
                 </div>
