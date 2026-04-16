10c78ef5977a9085e2435cc0f9f230cbec26626d
Mateus Balduino <balduinomateus0@gmail.com>
2026-04-15 21:15:46 -0300

    feat(p1): adiciona variáveis de useState no componente CapturaDados

--- src/CapturaDados.jsx
+++ src/CapturaDados.jsx
@@ -1,7 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useState } from 'react';
 
 function CapturaDados() {
+
+    const [valorInicial, setValorInicial] = useState(0);
+    const [taxaJuros, setTaxaJuros] = useState(0);
+    const [aporteMensal, setAporteMensal] = useState(0);
+    const [periodoMeses, setPeriodoMeses] = useState(0);
+
     return (
         <div className="container d-flex flex-column gap-4 text-start justify-content-start">
             <div className="row">
@@ -11,18 +18,26 @@ function CapturaDados() {
             <div className="row">
                 <div className="col-6">
                     <label className="form-label small">Valor Inicial (R$)</label>
-                    <input name="valor-inicial" className="form-control" type="text"></input>
+                    <input name="valor-inicial" className="form-control" type="text" value={valorInicial} onChange={(e) =>
+                        setValorInicial(e.target.value)
+                    }></input>
 
                     <label className="form-label mt-2 small">Taxa de Juros (% ao mês)</label>
-                    <input name="taxa-de-juros" className="form-control" type="text"></input>
+                    <input name="taxa-de-juros" className="form-control" type="text" value={taxaJuros} onChange={(e) =>
+                        setTaxaJuros(e.target.value)
+                    }></input>
                 </div>
 
                 <div className="col-6">
                     <label className="form-label small">Aporte Mensal (R$)</label>
-                    <input name="aporte-mensal" className="form-control" type="text"></input>
+                    <input name="aporte-mensal" className="form-control" type="text" value={aporteMensal} onChange={(e) =>
+                        setAporteMensal(e.target.value)
+                    }></input>
 
                     <label className="form-label mt-2 small">Período (meses)</label>
-                    <input name="periodo" className="form-control" type="text"></input>
+                    <input name="periodo" className="form-control" type="text" value={periodoMeses} onChange={(e) =>
+                        setPeriodoMeses(e.target.value)
+                    }></input>
                 </div>
 
             </div>
